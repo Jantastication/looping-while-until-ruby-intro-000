29439c30fd8b9e3aa6dab3e12df7e0723bd6073d
Jantastication O <georginaoregx@gmail.com>
2018-09-20 22:20:28 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,7 @@
 def using_while
-  levitation_force > 10
-  while levitation force < 6
+  levitation_force = 6
+  while levitation force < 10
     levitation_force += 1
   puts phrase "Wingardium Leviosa"
   end
+end
